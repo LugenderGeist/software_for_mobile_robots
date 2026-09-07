800771877c1fc919469b4f4d39569bddaeb3613d
LugenderGeist <avk270@tpu.ru>
2026-09-07 18:25:40 +0700

    import

---
+++ main.py
@@ -0,0 +1,16 @@
+import os  # Встроенная библиотека для работы с операционной системой
+import time  # Встроенная библиотека для работы со временем
+import zipfile  # Встроенная библиотека для работы с архивами
+
+import numpy as np  # Для работы с массивами и матрицами
+
+# Фреймворк для работы с нейронными сетями и продвинутыми моделями МО (содержит Keras)
+import tensorflow as tf
+import visualkeras  # Для визуализации архитектуры модели
+
+# Для оценки качества модели
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+
+import matplotlib.pyplot as plt  # Для визуализации графиков
+import seaborn as sns  # Для визуализации графиков с более красивым оформлением
+
